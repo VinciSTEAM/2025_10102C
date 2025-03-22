1a8af077b228676007a8d4dd246d07474155c19d
Shuai Yuan <syuan@vincirobotics.org>
2025-03-22 18:41:42 -0400

    us open

--- include/subsystems/conveyor.hpp
+++ include/subsystems/conveyor.hpp
@@ -1,7 +1,10 @@
 #pragma once
 
+#include "pros/optical.hpp"
 #include "lemlib/timer.hpp"
+#include "pros/rtos.hpp"
 #include "spinner.hpp"
+#include "subsystem.hpp"
 #include <cmath>
 
 namespace ConveyorNamespace {
@@ -10,7 +13,7 @@ enum class State { STOP, FORWARDS, REVERSE, INTAKE_FORWARD };
 
 enum Color { RED = 0, BLUE = 1 };
 
-class Conveyor : public subsystem<ConveyorNamespace::State> {
+class Conveyor : public subsystem<State, Conveyor> {
     public:
         Conveyor(SpinnerNamespace::Spinner* intake, SpinnerNamespace::Spinner* hooks,
                  std::shared_ptr<pros::Optical> optical_sensor, std::shared_ptr<pros::Distance> distance)
@@ -55,6 +58,8 @@ class Conveyor : public subsystem<ConveyorNamespace::State> {
         // Get the initial wrong hue value
         Color getInitColor() const { return init_color_; }
 
+        lemlib::Timer getTimer() { return timer; }
+
         // Get the optical sensor object
         std::shared_ptr<pros::Optical> getOpticalSensor() { return optical_sensor_; }
 
@@ -70,18 +75,68 @@ class Conveyor : public subsystem<ConveyorNamespace::State> {
         bool is_reversed() { return reverse_; }
 
         // Helper function to move the conveyor
-        void moveConveyor(SpinnerNamespace::State state) {
-            if (reverse_ and distance_ != nullptr and distance_->get_distance() < DIST_THRESHOLD and
-                state == SpinnerNamespace::State::FORWARD) {
-                intake_->moveToState(SpinnerNamespace::State::BACKWARD, 300);
-                hooks_->moveToState(SpinnerNamespace::State::BACKWARD, 300);
-                reverse_ = false;
-                return;
-            }
+        // void moveConveyor(SpinnerNamespace::State state) {
+        //     if (reverse_ and distance_ != nullptr and distance_->get_distance() < DIST_THRESHOLD and
+        //         state == SpinnerNamespace::State::FORWARD) {
+        //         intake_->moveToState(SpinnerNamespace::State::BACKWARD);
+        //         hooks_->moveToState(SpinnerNamespace::State::BACKWARD);
+        //         pros::delay(150);
+        //         moveToState(ConveyorNamespace::State::REVERSE, 180);
+        //         reverse_ = false;
+        //         return;
+        //     }
 
-            intake_->moveToState(state); // Move forward normally
-            hooks_->moveToState(state); // Move forward normally
+        //     intake_->moveToState(state); // Move forward normally
+        //     hooks_->moveToState(state); // Move forward normally
+        // }
+        void runTask() {
+
+            if (currState == ConveyorNamespace::State::FORWARDS) {
+                if (enable_color_sensor_ && detectWrongRing()) reverse_ = true;
+                if (reverse_ && distance_ != nullptr && distance_->get_distance() < DIST_THRESHOLD) {
+                    intake_->moveToState(SpinnerNamespace::State::FORWARD);
+                    hooks_->moveToState(SpinnerNamespace::State::FORWARD);
+                    pros::delay(140);
+                    intake_->moveToState(SpinnerNamespace::State::BACKWARD);
+                    hooks_->moveToState(SpinnerNamespace::State::BACKWARD);
+                    pros::delay(150);
+                    reverse_ = false;
+                    return;
+                }
+                intake_->moveToState(SpinnerNamespace::State::FORWARD);
+                hooks_->moveToState(SpinnerNamespace::State::FORWARD);
+            }
+            if (currState == ConveyorNamespace::State::REVERSE) {
+                intake_->moveToState(SpinnerNamespace::State::BACKWARD);
+                hooks_->moveToState(SpinnerNamespace::State::BACKWARD);
+            } 
+            if (currState == ConveyorNamespace::State::STOP) {
+                intake_->moveToState(SpinnerNamespace::State::IDLE);
+                hooks_->moveToState(SpinnerNamespace::State::IDLE);
+            }
+            // if (hooks_->getStuckedStatus() && hooks_->getCurrentState() == SpinnerNamespace::State::FORWARD) {
+            //     hooks_->moveToState(SpinnerNamespace::State::BACKWARD);
+            //     return;
+            // }            
+            // if (enable_color_sensor_ && detectWrongRing()) reverse_ = true;
+
+            // // Handle timed states
+            // if (timer.isDone() and timer.getTimeSet() > 0) {
+            //     moveToState(ConveyorNamespace::State::FORWARDS);
+            //     timer.set(0);
+            // }
+
+            // switch (currState) {
+            //     case ConveyorNamespace::State::FORWARDS: moveConveyor(SpinnerNamespace::State::FORWARD); break;
+            //     case ConveyorNamespace::State::REVERSE: moveConveyor(SpinnerNamespace::State::BACKWARD); break;
+            //     case ConveyorNamespace::State::STOP: moveConveyor(SpinnerNamespace::State::IDLE); break;
+            //     case ConveyorNamespace::State::INTAKE_FORWARD:
+            //         intake_->moveToState(SpinnerNamespace::State::FORWARD);
+            //         hooks_->moveToState(SpinnerNamespace::State::IDLE);
+            //         break;
+            // }
         }
+    
     private:
         SpinnerNamespace::Spinner* intake_;
         SpinnerNamespace::Spinner* hooks_;
@@ -90,26 +145,6 @@ class Conveyor : public subsystem<ConveyorNamespace::State> {
         Color init_color_;
         bool enable_color_sensor_ = true;
         bool reverse_ = false;
-
-        // Task that runs based on the current state
-        void runTask() override final {
-            if (enable_color_sensor_ and detectWrongRing()) reverse_ = true;
-
-            // Handle timed states
-            if (timer.isDone() and timer.getTimeSet() > 0) {
-                moveToState(ConveyorNamespace::State::STOP);
-                timer.set(0);
-            }
-
-            switch (currState) {
-                case ConveyorNamespace::State::FORWARDS: moveConveyor(SpinnerNamespace::State::FORWARD); break;
-                case ConveyorNamespace::State::REVERSE: moveConveyor(SpinnerNamespace::State::BACKWARD); break;
-                case ConveyorNamespace::State::STOP: moveConveyor(SpinnerNamespace::State::IDLE); break;
-                case ConveyorNamespace::State::INTAKE_FORWARD:
-                    intake_->moveToState(SpinnerNamespace::State::FORWARD);
-                    hooks_->moveToState(SpinnerNamespace::State::IDLE);
-                    break;
-            }
-        }
-};
+    
+    };
 } // namespace ConveyorNamespace
